738e7dcab82adb8d65f3aa78361d3958729d8eec
Matt Weldon <matt@mattweldon.com>
2018-02-11 10:22:09 +0000

    Test: Add placeholder tests for basic functionality

--- test/sesame_test.exs
+++ test/sesame_test.exs
@@ -2,7 +2,51 @@ defmodule SesameTest do
   use ExUnit.Case
   doctest Sesame
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  describe "signing a resource" do
+
+    test "will return a map if the user is permitted to sign the resource" do
+
+      result = Sesame.sign("http://wearesauce.io", %{name: "Matt Weldon", id: 1})
+
+      assert result[:signature]
+      assert result[:resource]
+      assert result[:signed_resource]
+    end
+
+    test "will return an :error atom if the user is not permitted to sign the resource" do
+      result = Sesame.sign("http://wearesauce.io", %{name: "John Polling", id: 2})
+
+      assert result == :error
+    end
+
+    test "will return a valid jwt" do
+      result = Sesame.sign("http://wearesauce.io", %{name: "Matt Weldon", id: 1})
+
+      signature = result[:signature]
+
+      # TODO: Add logic to decode a JWT
+    end
+
   end
+
+  describe "verify_signature" do
+    
+    test "will return an :ok atom if the signature can be verified" do 
+      signing_result = Sesame.sign("http://wearesauce.io", %{name: "Matt Weldon", id: 1})
+      
+      verification_result = Sesame.verify(signing_result[:signature], "http://wearesauce.io")
+
+      assert verification_result == :ok
+    end
+
+    test "will return an :error atom if the signature cannot be verified" do 
+      signing_result = Sesame.sign("http://wearesauce.io", %{name: "Matt Weldon", id: 1})
+      
+      verification_result = Sesame.verify(signing_result[:signature], "http://google.com")
+
+      assert verification_result == :error
+    end
+
+  end
+
 end
