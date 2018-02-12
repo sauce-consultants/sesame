4fedb3be848f53fee35d0f3354d64a489a31985e
Matt Weldon <matt@mattweldon.com>
2018-02-12 00:52:04 +0000

    Chore: Lower required version of poison

--- mix.exs
+++ mix.exs
@@ -48,7 +48,7 @@ defmodule Sesame.Mixfile do
   defp deps do
     [
       {:joken, "~> 1.5"},
-      {:poison, "~> 3.1"},
+      {:poison, "~> 2.0"},
       {:plug, "~> 1.4"},
       {:ex_doc, ">= 0.0.0", only: :dev}
     ]
